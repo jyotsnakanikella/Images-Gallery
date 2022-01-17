b72ab7c43e58ce13125c5f4678cd3399ee443457
chelsea <kcjyotsna999@gmail.com>
2022-01-16 23:29:56 -0800

    Adding searchimage feature with controlled search component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { Navbar } from "react-bootstrap";
+import { Navbar, Container } from "react-bootstrap";
+
+const navbarStyle = {
+  backgroundColor: "lightblue",
+};
 
 const Header = (props) => {
-  const {title} = props;
+  const { title } = props;
   return (
-    <Navbar bg="light" variant="light">
-      <Navbar.Brand href="/">{title}</Navbar.Brand>
+    <Navbar style={navbarStyle} variant="light">
+      <Container>
+        <Navbar.Brand href="/">{title}</Navbar.Brand>
+      </Container>
     </Navbar>
   );
 };
