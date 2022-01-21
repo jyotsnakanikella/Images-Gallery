c5c85f5ff90be6b7be0d882e72c359369badc0cb
chelsea <kcjyotsna999@gmail.com>
2022-01-21 03:10:20 -0800

    enable pylint,black,pycodestyle formatter

--- api/main.py
+++ api/main.py
@@ -9,40 +9,41 @@ from other_module import fn_from_other_module
 from dotenv import load_dotenv
 from flask_cors import CORS
 
-#adding .env.local to os.environ
+# adding .env.local to os.environ
 load_dotenv(dotenv_path="./.env.local")
-UNSPLASH_URL = 'https://api.unsplash.com/photos/random'
-UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY","")
-DEBUG = bool(os.environ.get("DEBUG",True))
+
+UNSPLASH_URL = "https://api.unsplash.com/photos/random"
+UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
+DEBUG = bool(os.environ.get("DEBUG", True))
 
 if not UNSPLASH_KEY:
-  raise EnvironmentError("Please create .env.local file and insert UNSPLASH_KEY")
+    raise EnvironmentError("Please create .env.local file and insert UNSPLASH_KEY")
 
 app = Flask(__name__)
 CORS(app)
 app.config["DEBUG"] = DEBUG
 # DEBUG="" add in .env.local if you want debug=false
 
+
 @app.route("/")
 def hello():
     return "Hello world"
-  
-@app.route("/new-image")  
+
+
+@app.route("/new-image")
 def new_image():
     word = request.args.get("query")
-    headers = {
-      "Accept-Version":"v1",
-      "Authorization": f"Client-ID {UNSPLASH_KEY}"
-    }
-    payload = {"query":f"{word}"}
-    response = requests.get(url=UNSPLASH_URL,headers=headers,params=payload)
-    data = response.json() 
+    headers = {"Accept-Version": "v1", "Authorization": f"Client-ID {UNSPLASH_KEY}"}
+    payload = {"query": f"{word}"}
+    response = requests.get(url=UNSPLASH_URL, headers=headers, params=payload)
+    data = response.json()
     return data
-  
-if __name__ == '__main__':
-  app.run("0.0.0.0",5050) 
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", 5050)
 
 # def hello():
 #     return "Hello world"
-  
-# app.route('/')(hello)  
+
+# app.route('/')(hello)
