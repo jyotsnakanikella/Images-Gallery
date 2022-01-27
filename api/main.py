7fce5eb704d77a386c0fd2a0f2ab1ef8bd44387d
chelsea <kcjyotsna999@gmail.com>
2022-01-27 00:35:26 -0800

    basic images endpoint with get and post methods

--- api/main.py
+++ api/main.py
@@ -4,13 +4,17 @@
 # $env:FLASK_APP = "hello.py"
 # flask run
 import os
+from itsdangerous import json
 import requests
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 # from other_module import fn_from_other_module
 from dotenv import load_dotenv
 from flask_cors import CORS
-from mongo_client import insert_test_document
+from mongo_client import mongo_client
+
+gallery = mongo_client.gallery
+images_collection = gallery.images
 
 # adding .env.local to os.environ
 load_dotenv(dotenv_path="./.env.local")
@@ -20,13 +24,12 @@ UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
 DEBUG = bool(os.environ.get("DEBUG", True))
 
 if not UNSPLASH_KEY:
-    raise EnvironmentError("Please create .env.local file and insert UNSPLASH_KEY")
+    raise EnvironmentError("create .env.local file & add UNSPLASH_KEY")
 
 app = Flask(__name__)
 CORS(app)
 app.config["DEBUG"] = DEBUG
 # DEBUG="" add in .env.local if you want debug=false
-insert_test_document()
 
 
 @app.route("/")
@@ -47,6 +50,23 @@ def new_image():
     response = requests.get(url=UNSPLASH_URL, headers=headers, params=payload)
     data = response.json()
     return data
+
+
+@app.route("/images", methods=["GET", "POST"])
+def images():
+    if request.method == "GET":
+        # read images from db
+        # returns cursor
+        images = images_collection.find({})
+        return jsonify([img for img in images])
+
+    if request.method == "POST":
+        # save image in db
+        image = request.get_json()
+        image["_id"] = image.get("id")
+        result = images_collection.insert_one(image)
+        inserted_id = result.inserted_id
+        return {"inserted_id": inserted_id}
 
 
 if __name__ == "__main__":
